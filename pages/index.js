93a6b8a48ea8ec6fa4d756e36680536d8446c59b
dinhvu-agl <a30014365@agl.com.au>
2023-03-31 11:52:50 +1100

    format

--- pages/index.js
+++ pages/index.js
@@ -3,28 +3,30 @@ import { projectIcons } from '../components/Icons';
 
 import { projects } from '../utils/projectsData';
 
-const Home = ({ data  }) => {
+const Home = ({ data }) => {
   const serverData = JSON.parse(data);
   return (
-  <div className="home">
-    <h1>What Can I Deploy to Static Apps?</h1>
-    <h1 className={styles.title}>
-      Welcome to <a href="https://nextjs.org">Next.js! The time is {serverData.time}</a>
-  </h1>
-    <div className="card-grid">
-      {projects.map((project) => {
-        const Icon = projectIcons[project.id];
-        return (
-          <SmallCard
-            key={project.id}
-            Icon={Icon}
-            title={project.name}
-            slug={project.slug}
-          />
-        );
-      })}
+    <div className="home">
+      <h1>What Can I Deploy to Static Apps?</h1>
+      <h1 className={styles.title}>
+        Welcome to{' '}
+        <a href="https://nextjs.org">Next.js! The time is {serverData.time}</a>
+      </h1>
+      <div className="card-grid">
+        {projects.map((project) => {
+          const Icon = projectIcons[project.id];
+          return (
+            <SmallCard
+              key={project.id}
+              Icon={Icon}
+              title={project.name}
+              slug={project.slug}
+            />
+          );
+        })}
+      </div>
     </div>
-  </div>)
+  );
 };
 
 export default Home;
