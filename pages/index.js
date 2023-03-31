28bbe865fa36e0ba507f64cad1634d53a6ee84e0
dinhvu-agl <a30014365@agl.com.au>
2023-03-31 11:49:51 +1100

    server-render data

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,14 @@ import { projectIcons } from '../components/Icons';
 
 import { projects } from '../utils/projectsData';
 
-const Home = () => (
+const Home = ({ data  }) => {
+  const serverData = JSON.parse(data);
+  return (
   <div className="home">
     <h1>What Can I Deploy to Static Apps?</h1>
+    <h1 className={styles.title}>
+      Welcome to <a href="https://nextjs.org">Next.js! The time is {serverData.time}</a>
+  </h1>
     <div className="card-grid">
       {projects.map((project) => {
         const Icon = projectIcons[project.id];
@@ -19,7 +24,12 @@ const Home = () => (
         );
       })}
     </div>
-  </div>
-);
+  </div>)
+};
 
 export default Home;
+
+export async function getServerSideProps() {
+  const data = JSON.stringify({ time: new Date() });
+  return { props: { data } };
+}
